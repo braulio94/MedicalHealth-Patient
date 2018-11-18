906dc374a7c5ce2f0e9009b30be69897b1f5a920
Braulio Cassule <brauliocassule94@gmail.com>
2018-11-18 21:40:02 +0100

    make register and login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,8 +3,68 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+        hintText: 'Email',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
 
+    final password = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Palavra-passa',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final loginButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(30.0),
+        shadowColor: Colors.lightBlueAccent.shade100,
+        elevation: 5.0,
+        child: MaterialButton(
+          minWidth: 200.0,
+          height: 42.0,
+          onPressed: () {
+
+          },
+          color: Colors.lightBlueAccent,
+          child: Text('Entrar', style: TextStyle(color: Colors.white)),
+        ),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            Text('Medical Health - Patient'),
+            SizedBox(height: 48.0),
+            email,
+            SizedBox(height: 8.0),
+            password,
+            SizedBox(height: 24.0),
+            loginButton,
+            FlatButton(
+              child: Text(
+                'Esqueceu palavra-passa?',
+                style: TextStyle(color: Colors.black54),
+              ),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -3,8 +3,67 @@ import 'package:flutter/material.dart';
 class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
 
+    final registerButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(30.0),
+        shadowColor: Colors.lightBlueAccent.shade100,
+        elevation: 5.0,
+        child: MaterialButton(
+          minWidth: 200.0,
+          height: 42.0,
+          onPressed: () {
+
+          },
+          color: Colors.lightBlueAccent,
+          child: Text('Registrar', style: TextStyle(color: Colors.white)),
+        ),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            Text('Medical Health - Patient'),
+            SizedBox(height: 48.0),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              autofocus: false,
+              decoration: InputDecoration(
+                hintText: 'Email',
+                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+              ),
+            ),
+            SizedBox(height: 8.0),
+            TextFormField(
+              autofocus: false,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Palavra-passa',
+                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+              ),
+            ),
+            TextFormField(
+              autofocus: false,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Nova palavra-passa',
+                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+              ),
+            ),
+            SizedBox(height: 24.0),
+            registerButton,
+          ],
+        ),
+      ),
     );
   }
 }
