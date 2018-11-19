2a7b94f479453c44c7d04c68b442fabc0e93ec7d
Braulio Cassule <brauliocassule94@gmail.com>
2018-11-19 03:30:47 +0100

    generate and view qr code in qr viewer page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -45,7 +45,7 @@ class RegisterPage extends StatelessWidget {
               autofocus: false,
               obscureText: true,
               decoration: InputDecoration(
-                hintText: 'Palavra-passa',
+                hintText: 'Nova palavra-passa',
                 contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
               ),
@@ -54,7 +54,7 @@ class RegisterPage extends StatelessWidget {
               autofocus: false,
               obscureText: true,
               decoration: InputDecoration(
-                hintText: 'Nova palavra-passa',
+                hintText: 'Confirmar palavra-passa',
                 contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
               ),
