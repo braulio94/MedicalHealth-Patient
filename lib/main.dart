e288e13e42ea4ac4c718d1fb14da13571d78dc72
Braulio Cassule <brauliocassule94@gmail.com>
2018-11-22 19:55:36 +0100

    add missing files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:medical_health_patient/pages/form_page.dart';
+import 'package:medical_health_patient/pages/login_page.dart';
 
 void main() => runApp(MedicalHealth());
 
@@ -11,7 +12,7 @@ class MedicalHealth extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: FormPage(),
+      home: LoginPage(),
     );
   }
 }
